6f53ee42fad0837a9b2a41badcf9936e7faa0289
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-01-10 20:36:23 +0200

    add MyCroft sidebar and custom webview with basic HTML content

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "mycroft" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mycroft" is now active!');
+  const disposable = vscode.commands.registerCommand('mycroft.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from MyCroft! with view');
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mycroft.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MyCroft! Ron you are in trouble');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  vscode.window.registerWebviewViewProvider('mycroftView', new MyCroftViewProvider(context));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+class MyCroftViewProvider implements vscode.WebviewViewProvider {
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveWebviewView(webviewView: vscode.WebviewView) {
+    webviewView.webview.options = {
+      enableScripts: true,
+    };
+
+    webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+  }
+
+  private getHtmlForWebview(webview: vscode.Webview): string {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MyCroft View</title>
+      </head>
+      <body>
+        <h1>Hello from MyCroft!</h1>
+        <p>This is your custom view content.</p>
+      </body>
+      </html>
+    `;
+  }
+}
